120795d1659d65851a618049331e249210ca8575
kairoslav <svt210@yandex.ru>
2020-10-17 16:10:08 +0300

    Add: MD Generator

---
+++ leetcode/MDGenerator/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(final String[] args) throws Throwable {
+        try {
+            LeetCodeSourceData File = new LeetCodeSourceData(args[0]);
+            List<Integer> a = new LinkedList<>();
+            File.writeToMDFile(args[1]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(String.format("Invalid number of parameters\n%d of 2 arguments recieved", args.length));
+        }
+    }
+}
