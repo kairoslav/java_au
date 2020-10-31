246379f5392b7dca1a65c1ccabe73212d47e62c1
kairoslav <svt210@yandex.ru>
2020-10-31 20:25:02 +0300

    Reformat: beautification is applied

--- src/MDGenerator/LeetCodeSourceData.java
+++ src/MDGenerator/LeetCodeSourceData.java
@@ -16,76 +16,75 @@ public class LeetCodeSourceData {
 
     public LeetCodeSourceData(String txtFilePath) throws Throwable {
         List<String> data = ReadWriteUtils.read(txtFilePath);
+
         if (data.isEmpty()) {
             throw new FileNotFoundException("Txt file hasn't found");
         } else {
-            this.title = data.get(0).replaceFirst("[0-9]*. ", "");
+            this.title        = data.get(0).replaceFirst("[0-9]*. ", "");
             this.leetcodeLink = data.get(1);
-            this.solution = data.stream()
-                    .skip(2)
-                    .map(x -> x.replaceFirst(" {4}", "") + "\n")
-                    .collect(Collectors.joining());
+            this.solution     = data.stream()
+                                    .skip(2)
+                                    .map(x -> x.replaceFirst(" {4}", "") + "\n")
+                                    .collect(Collectors.joining());
         }
     }
 
     private static String firstCharUpper(String s) {
         StringBuilder str = new StringBuilder(s);
+
         str.setCharAt(0, s.charAt(0));
+
         return str.toString();
     }
 
-    private String getMDSolutionLink() {
-        return String.format("+ [%s](#%s)", title, title.toLowerCase().replace(' ', '-'));
-    }
-
     public void writeToMDFile(String mdFilePath) throws IOException {
-        String commentMDLink = "[MDLink]: <>";
-        String commentSolution = "[Solution]: <>";
-        StringBuilder finalResult = new StringBuilder();
+        String        commentMDLink   = "[MDLink]: <>";
+        String        commentSolution = "[Solution]: <>";
+        StringBuilder finalResult     = new StringBuilder();
+
         if (!Files.exists(Path.of(mdFilePath)) || (Files.size(Path.of(mdFilePath)) == 0)) {
             finalResult.append("# ")
-                    .append(firstCharUpper(mdFilePath.replace(".md", "")))
-                    .append("\n\n")
-                    .append(getMDSolutionLink())
-                    .append("\n\n")
-                    .append(commentMDLink)
-                    .append("\n\n## ")
-                    .append(title)
-                    .append("\n\n")
-                    .append(leetcodeLink)
-                    .append("\n\n```java\n")
-                    .append(solution)
-                    .append("```\n\n")
-                    .append(commentSolution);
-
+                       .append(firstCharUpper(mdFilePath.replace(".md", "")))
+                       .append("\n\n")
+                       .append(getMDSolutionLink())
+                       .append("\n\n")
+                       .append(commentMDLink)
+                       .append("\n\n## ")
+                       .append(title)
+                       .append("\n\n")
+                       .append(leetcodeLink)
+                       .append("\n\n```java\n")
+                       .append(solution)
+                       .append("```\n\n")
+                       .append(commentSolution);
             ReadWriteUtils.write(mdFilePath, finalResult.toString());
         } else {
             List<String> MDFile = ReadWriteUtils.read(mdFilePath);
 
             for (String line : MDFile) {
                 if (line.equals(commentMDLink)) {
-                    finalResult.append(getMDSolutionLink())
-                            .append("\n\n")
-                            .append(commentMDLink)
-                            .append("\n");
+                    finalResult.append(getMDSolutionLink()).append("\n\n").append(commentMDLink).append("\n");
                 } else if (line.equals(commentSolution)) {
                     finalResult.append("## ")
-                            .append(title)
-                            .append("\n\n")
-                            .append(leetcodeLink)
-                            .append("\n\n")
-                            .append("```java\n")
-                            .append(solution)
-                            .append("```")
-                            .append("\n\n")
-                            .append(commentSolution);
+                               .append(title)
+                               .append("\n\n")
+                               .append(leetcodeLink)
+                               .append("\n\n")
+                               .append("```java\n")
+                               .append(solution)
+                               .append("```")
+                               .append("\n\n")
+                               .append(commentSolution);
                 } else {
-                    finalResult.append(line)
-                            .append("\n");
+                    finalResult.append(line).append("\n");
                 }
             }
 
             ReadWriteUtils.write(mdFilePath, finalResult.toString());
         }
+    }
+
+    private String getMDSolutionLink() {
+        return String.format("+ [%s](#%s)", title, title.toLowerCase().replace(' ', '-'));
     }
 }
