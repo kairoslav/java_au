4ddfa2353649e8edf2ef59191d6dec1bff527d41
kairoslav <svt210@yandex.ru>
2020-10-31 20:20:39 +0300

    Reformat: String.format() is changed to StringBuilder.append()
    //https://redfin.engineering/java-string-concatenation-which-way-is-best-8f590a7d22a8

--- src/MDGenerator/LeetCodeSourceData.java
+++ src/MDGenerator/LeetCodeSourceData.java
@@ -16,11 +16,10 @@ public class LeetCodeSourceData {
 
     public LeetCodeSourceData(String txtFilePath) throws Throwable {
         List<String> data = ReadWriteUtils.read(txtFilePath);
-
         if (data.isEmpty()) {
             throw new FileNotFoundException("Txt file hasn't found");
         } else {
-            this.title        = data.get(0).replaceFirst("[0-9]*. ", "");
+            this.title = data.get(0).replaceFirst("[0-9]*. ", "");
             this.leetcodeLink = data.get(1);
             this.solution = data.stream()
                     .skip(2)
@@ -31,50 +30,62 @@ public class LeetCodeSourceData {
 
     private static String firstCharUpper(String s) {
         StringBuilder str = new StringBuilder(s);
-
         str.setCharAt(0, s.charAt(0));
-
         return str.toString();
     }
 
+    private String getMDSolutionLink() {
+        return String.format("+ [%s](#%s)", title, title.toLowerCase().replace(' ', '-'));
+    }
+
     public void writeToMDFile(String mdFilePath) throws IOException {
-        String commentMDLink   = "[MDLink]: <>";
+        String commentMDLink = "[MDLink]: <>";
         String commentSolution = "[Solution]: <>";
-
+        StringBuilder finalResult = new StringBuilder();
         if (!Files.exists(Path.of(mdFilePath)) || (Files.size(Path.of(mdFilePath)) == 0)) {
-            String finalResult = String.format("# %s\n\n%s\n\n%s\n\n## %s\n\n%s\n\n```java\n%s```\n\n%s",
-                                               firstCharUpper(mdFilePath.replace(".md", "")),
-                                               getMDSolutionLink(),
-                                               commentMDLink,
-                                               title,
-                                               leetcodeLink,
-                                               solution,
-                                               commentSolution);
+            finalResult.append("# ")
+                    .append(firstCharUpper(mdFilePath.replace(".md", "")))
+                    .append("\n\n")
+                    .append(getMDSolutionLink())
+                    .append("\n\n")
+                    .append(commentMDLink)
+                    .append("\n\n## ")
+                    .append(title)
+                    .append("\n\n")
+                    .append(leetcodeLink)
+                    .append("\n\n```java\n")
+                    .append(solution)
+                    .append("```\n\n")
+                    .append(commentSolution);
 
-            ReadWriteUtils.write(mdFilePath, finalResult);
+            ReadWriteUtils.write(mdFilePath, finalResult.toString());
         } else {
-            List<String>  MDFile      = ReadWriteUtils.read(mdFilePath);
-            StringBuilder finalResult = new StringBuilder();
+            List<String> MDFile = ReadWriteUtils.read(mdFilePath);
 
             for (String line : MDFile) {
                 if (line.equals(commentMDLink)) {
-                    finalResult.append(getMDSolutionLink()).append("\n\n").append(commentMDLink).append("\n");
+                    finalResult.append(getMDSolutionLink())
+                            .append("\n\n")
+                            .append(commentMDLink)
+                            .append("\n");
                 } else if (line.equals(commentSolution)) {
-                    finalResult.append(String.format("## %s", title)).append("\n\n");
-                    finalResult.append(leetcodeLink).append("\n\n");
-                    finalResult.append("```java\n").append(solution);
-                    finalResult.append("```").append("\n\n");
-                    finalResult.append(commentSolution);
+                    finalResult.append("## ")
+                            .append(title)
+                            .append("\n\n")
+                            .append(leetcodeLink)
+                            .append("\n\n")
+                            .append("```java\n")
+                            .append(solution)
+                            .append("```")
+                            .append("\n\n")
+                            .append(commentSolution);
                 } else {
-                    finalResult.append(line).append("\n");
+                    finalResult.append(line)
+                            .append("\n");
                 }
             }
 
             ReadWriteUtils.write(mdFilePath, finalResult.toString());
         }
-    }
-
-    private String getMDSolutionLink() {
-        return String.format("+ [%s](#%s)", title, title.toLowerCase().replace(' ', '-'));
     }
 }
